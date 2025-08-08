5e2951f9468903d03df16490b4f68ae3040b5dd5
DUY2602 <duydo7779@gmail.com>
2025-08-08 09:36:26 +0700

    upload bai-01

---
+++ bai-01.py
@@ -0,0 +1,10 @@
+# Bài 1: Nhập vào diện tích S của 1 mặt cầu. Tính thể tích V của mặt cầu đó
+
+import math
+
+dientich = float(input("Nhập diện tích S: "))
+Pi = 3.141593
+bankinh = math.sqrt(dientich / (4*Pi))
+
+thetich = 4/3 * Pi * bankinh**3
+print(f"Thể tích V của mặt cầu là: {thetich}")
